617a9df2edd4696d6ebe495a57febab2ccfb1a95
sophieO1970 <sophieO1970@gmail.com>
2020-10-04 11:35:28 +0300

    generate components for dispalying github.search form for searching the users,home page for my github and a navbar to help in vieiwng the page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchformComponent } from './searchform/searchform.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GithubComponent } from './github/github.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchformComponent,
+    NavbarComponent,
+    GithubComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
