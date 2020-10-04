87c529c71478933eca02f90b383187f6875e9ced
sophieO1970 <sophieO1970@gmail.com>
2020-10-04 21:09:29 +0300

    import user,repo and searchreposervice to user-repos,add error handler

--- src/app/user-repos/user-repos.component.ts
+++ src/app/user-repos/user-repos.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
+import { SearchRepoService } from '../search-repo-service/search-repo.service';
+import { User } from '../user';
+import { Repo } from '../repo';
+
 
 @Component({
   selector: 'app-user-repos',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserReposComponent implements OnInit {
 
-  constructor() { }
+  @Input() user: User;
+  repos: Repo[];
+
+  constructor(private searchRepoService: SearchRepoService) {}
+
+
+
+  getUserRepos(endpoint) {
+    this.searchRepoService.getUserRepos(endpoint).then(
+      () => {
+        this.repos = this.searchRepoService.userRepos;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 
   ngOnInit(): void {
+    this.getUserRepos(this.user.reposUrl);
   }
 
 }
