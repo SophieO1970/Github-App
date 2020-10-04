66e00c0694a4abc294746185d8ab96a7caece9fa
sophieO1970 <sophieO1970@gmail.com>
2020-10-04 14:56:35 +0300

    define a method to get user,specify on ngoninit the name of the person to be displayed

--- src/app/github/github.component.ts
+++ src/app/github/github.component.ts
@@ -11,11 +11,41 @@ import { from } from 'rxjs';
   styleUrls: ['./github.component.css']
 })
 export class GithubComponent implements OnInit {
-  user: User[];
+  users: User[];
+  repos: Repo[];
+  usersLength: number;
+  reposLength: number;
 
-  constructor( public searchUserService: SearchUserService,) { }
+
+  constructor( 
+    public searchUserService: SearchUserService,
+    public searchRepoService: SearchRepoService) { }
+
+
+    find(term: string) {
+      this.searchUserService.getUsers(term).then(
+        () => {
+          this.users = this.searchUserService.usersGotten;
+          this.usersLength = this.searchUserService.usersGotten.length;
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+      this.searchRepoService.getRepos(term).then(
+        () => {
+          this.repos = this.searchRepoService.ReposGotten;
+          this.reposLength = this.searchRepoService.ReposGotten.length;
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
+  
 
   ngOnInit(): void {
+    this.find ('sophia')
   }
 
 }
