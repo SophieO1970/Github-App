f22e280ecfae88243864ef814ce1cbbb47e8cdc7
sophieO1970 <sophieO1970@gmail.com>
2020-10-04 17:56:35 +0300

    add a prototype for the user,error handler,http method to help in fecthing user information,use the promise to help in asychronous programming

--- src/app/search-repo-service/search-repo.service.ts
+++ src/app/search-repo-service/search-repo.service.ts
@@ -1,25 +1,26 @@
 import { Injectable } from '@angular/core';
 import { Repo } from '../repo';
 import { HttpClient } from '@angular/common/http';
-import { environment} from '../../environments/environment';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchRepoService {
   ReposGotten: Repo[];
+  userRepos: Repo[];
 
   getRepos(term: string) {
     let endpoint = `https://api.github.com/search/repositories?access token=${environment.apiKey}&q=${term}`
-    let promise = new Promise ((resolve, reject)=>{
+    let promise = new Promise((resolve, reject) => {
       this.http
-      .get(endpoint)
-      .toPromise()
-      .then(
-        (results) => {
-          this.ReposGotten = [];
-          for (let i = 0; i < results['items'].length; i++) {
-            let name = results['items'][i]['full_name'];
+        .get(endpoint)
+        .toPromise()
+        .then(
+          (results) => {
+            this.ReposGotten = [];
+            for (let i = 0; i < results['items'].length; i++) {
+              let name = results['items'][i]['full_name'];
               let description = results['items'][i]['description'];
               let language = results['items'][i]['language'];
               let year = parseInt(
@@ -43,21 +44,62 @@ export class SearchRepoService {
                 liveLink
               );
               this.ReposGotten.push(repo);
-          }
-          resolve ();
+            }
+            resolve();
 
-        },
-        (error)=>{
-          console.log(error);
-          reject();
-        }
-        
-      )
+          },
+          (error) => {
+            console.log(error);
+            reject();
+          }
+
+        )
 
     })
     return promise;
   }
 
 
-  constructor(private http: HttpClient) {}
+  getUserRepos(endpoint) {
+    let promise = new Promise((resolve, reject) => {
+      this.http
+        .get<Repo[]>(endpoint + '?access_token=' + environment.apiKey)
+        .toPromise()
+        .then(
+          (response) => {
+            this.userRepos = [];
+            for (let i = 0; i < response.length; i++) {
+              let name = response[i]['full_name'];
+              let description = response[i]['description'];
+              let language = response[i]['language'];
+              let year = parseInt(response[i]['created_at'].substr(0, 4));
+              let month = parseInt(response[i]['created_at'].substr(5, 7)) - 1;
+              let day = parseInt(response[i]['created_at'].substr(8, 10));
+              let date = new Date(year, month, day);
+              let gitHubLink = response[i]['html_url'];
+              let liveLink = response[i]['homepage'];
+              let repo = new Repo(
+                i + 1,
+                name,
+                description,
+                language,
+                date,
+                gitHubLink,
+                liveLink
+              );
+              this.userRepos.push(repo);
+            }
+
+            resolve();
+          },
+          (error) => {
+            console.log(error);
+
+            reject(error);
+          }
+        );
+    });
+    return promise;
+  }
+  constructor(private http: HttpClient) { }
 }
