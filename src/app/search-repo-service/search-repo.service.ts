d5aaae329996e3443b70086af59d90c9f79b6d5a
sophieO1970 <sophieO1970@gmail.com>
2020-10-04 14:39:27 +0300

    add end point for getting the repos,create the objects that will be passed once the repos are requested and loop through them

--- src/app/search-repo-service/search-repo.service.ts
+++ src/app/search-repo-service/search-repo.service.ts
@@ -19,22 +19,44 @@ export class SearchRepoService {
         (results) => {
           this.ReposGotten = [];
           for (let i = 0; i < results['items'].length; i++) {
-            
+            let name = results['items'][i]['full_name'];
+              let description = results['items'][i]['description'];
+              let language = results['items'][i]['language'];
+              let year = parseInt(
+                results['items'][i]['created_at'].substr(0, 4)
+              );
+              let month =
+                parseInt(results['items'][i]['created_at'].substr(5, 7)) - 1;
+              let day = parseInt(
+                results['items'][i]['created_at'].substr(8, 10)
+              );
+              let date = new Date(year, month, day);
+              let gitHubLink = results['items'][i]['html_url'];
+              let liveLink = results['items'][i]['homepage'];
+              let repo = new Repo(
+                i + 1,
+                name,
+                description,
+                language,
+                date,
+                gitHubLink,
+                liveLink
+              );
+              this.ReposGotten.push(repo);
           }
+          resolve ();
 
-        }
+        },
+        // (error)=>{
+        //   console.log(error);
+        //   reject();
+        // }
+        
       )
 
-
     })
+    return promise;
   }
-
-
-
-
-
-
-
 
 
   constructor(private http: HttpClient) { }
